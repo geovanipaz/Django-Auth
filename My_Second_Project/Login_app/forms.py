94f2ea2262fada1dd63ea90f0201bfec8e5822dc
Geovani Paz <geovanipaz7@gmail.com>
2022-03-18 18:01:02 -0300

    criação formulário de registro

---
+++ My_Second_Project/Login_app/forms.py
@@ -0,0 +1,16 @@
+from dataclasses import field, fields
+from pyexpat import model
+from django import forms
+from django.contrib.auth.models import User
+from Login_app.models import UserInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+        
+class UserInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserInfo
+        fields = ('facebook_id','profile_pic')
