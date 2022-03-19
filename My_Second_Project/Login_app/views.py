64595ad9c3f82adc7eb15e89a59d438ebfd0c812
Geovani Paz <geovanipaz7@gmail.com>
2022-03-19 10:14:44 -0300

    inicio do sistema de login

--- My_Second_Project/Login_app/views.py
+++ My_Second_Project/Login_app/views.py
@@ -1,16 +1,53 @@
 from django.shortcuts import render
 from Login_app.forms import UserForm, UserInfoForm
-# Create your views here.
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
+
+def login_page(request):
+    return render(request,'Login_app/login.html', context={})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
 def index(request):
     dict = {}
     return render(request, 'Login_app/index.html', dict)
 
 def register(request):
-    user_form = UserForm()
-    user_info_form = UserInfoForm()
+    
+    registered = False
+    
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        user_info_form = UserInfoForm(data=request.POST)
+        
+        if user_form.is_valid() and user_info_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            
+            user_info = user_info_form.save(commit=False)
+            user_info.user = user
+            
+            if 'profile_pic' in request.FILES:
+                user_info.profile_pic = request.FILES['profile_pic']
+                
+            user_info.save()
+            
+            registered = True
+    else:
+        user_form = UserForm()
+        user_info_form = UserInfoForm() 
+    
     dict = {
         'user_form':user_form,
         'user_info_form':user_info_form,
+        'registered': registered,
     }
     return render(request,'Login_app/register.html', dict)
